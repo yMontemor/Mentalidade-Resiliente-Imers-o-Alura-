a416144afbd1f26b0fe64fbc8eb3d6817bf595f5
yMontemor <bielmontemor7@gmail.com>
2024-09-07 17:08:51 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo: "Cristiano Ronaldo",
+        descricao: "Cristiano Ronaldo é conhecido não apenas por suas habilidades técnicas excepcionais em campo, mas também por sua mentalidade de campeão inigualável. Sua determinação, foco  e disciplina o impulsionaram a alcançar patamares inéditos no mundo do futebol",
+        link: "https://www.instagram.com/cristiano/",
+        idade: 39,
+        nacionalidade: "Portugal",
+        tags: "futebol, bola, dinheiro, habilidade, mentalidade, gols, familia"
+    },
+    {
+        titulo: "David Goggins",
+        descricao: "David Goggins é um ex-SEAL da Marinha dos Estados Unidos conhecido por sua incrível resistência física e mental. Ele é autor de best-sellers e palestrante motivacional, inspirando milhões de pessoas a ultrapassarem seus limites.",
+        link: "https://www.instagram.com/davidgoggins/",
+        idade: 49,
+        nacionalidade: "Norte Americano",
+        tags: "forte, seal, marinha, eua,"
+    },
+    {
+        titulo: "Conor McGregor",
+        descricao: "Conor McGregor é um lutador de MMA irlandês, conhecido por sua personalidade carismática e suas habilidades de combate impressionantes. Ele se tornou um dos atletas mais bem pagos do mundo.",
+        link: "https://www.instagram.com/thenotoriousmma/",
+        idade: 36,
+        nacionalidade: "Irlandês",
+        tags: "luta, forte, bravo, resiliente"
+    }
+];
+
